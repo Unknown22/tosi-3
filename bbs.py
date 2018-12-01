e191f3b10396e9d1711f58e69c3186752e6f428e
Marcin Migda <marcin700@gmail.com>
2018-12-01 08:44:38 +0100

    Removed unnecessary comments

--- bbs.py
+++ bbs.py
@@ -41,39 +41,3 @@ class BlumBlumShub():
                 break
         return p
     #
-    # def generate_n(self, bits):
-    #     p = self.get_prime(bits)
-    #     q = self.get_prime(bits)
-    #
-    #     while p == q:
-    #         q = self.get_prime(self, bits)
-    #
-    #     # print "GenerateN - p = " + repr(p) + ", q = " + repr(q)
-    #     return p * q
-    #
-    # def __init__(self, bits):
-    #     self.n = self.generate_n(bits)
-    #     length = self.bit_len(self.n)
-    #     seed = random.getrandbits(length)
-    #     self.set_seed(seed)
-    #
-    # def set_seed(self, seed):
-    #     self.state = seed % self.n
-    #
-    # def bit_len(self, x):
-    #     assert (x > 0)
-    #     q = 0
-    #     while x:
-    #         q = q + 1
-    #         x = x >> 1
-    #     return q
-    #
-    # def next(self, numBits):
-    #     "Returns up to numBit random bits"
-    #
-    #     result = 0
-    #     for i in range(numBits, 0, -1):
-    #         self.state = (self.state ** 2) % self.n
-    #         result = (result << 1) | (self.state & 1)
-    #
-    #     return result
