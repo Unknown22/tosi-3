5ace16c3eaa1bc8ac1d137c3aee66a33de357de3
Marcin Migda <marcin700@gmail.com>
2018-12-01 02:05:38 +0100

    Final version

--- main.py
+++ main.py
@@ -1,31 +1,17 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import binascii
-import sys
+import os
 from bbs import BlumBlumShub
 
 
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-    n = int(bits, 2)
-    return int2bytes(n).decode(encoding, errors)
-
-
-def int2bytes(i):
-    hex_string = '%x' % i
-    n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
-
-
 def process_vernam_cypher(text, key=None):
+    bin_text = ''.join('{0:010b}'.format(ord(x), 'b') for x in text)
     if key:
+        key = ''.join('{0:010b}'.format(ord(x), 'b') for x in key)
         p = 0
         answer = ""
-        for ch in text:
+        for ch in bin_text:
             answer += str(int(ch) ^ int(key[p]))
             p += 1
         n = 10
@@ -33,9 +19,8 @@ def process_vernam_cypher(text, key=None):
 
         answer = [chr(int(el, 2)) for el in answer]
         answer = "".join([el for el in answer])
-        key_bits = None
+        key_to_return = None
     else:
-        bin_text = ''.join('{0:010b}'.format(ord(x), 'b') for x in text)
         bbs_instance = BlumBlumShub()
         key_bits = bbs_instance.run_bbs(bits=512, len_of_message=len(bin_text))
         answer = ""
@@ -44,16 +29,38 @@ def process_vernam_cypher(text, key=None):
             answer += str(int(ch) ^ int(key_bits[p]))
             p += 1
 
-    return answer, key_bits
+        n = 10
+        tmp = [answer[i:i + n] for i in range(0, len(answer), n)]
+        tmp = [chr(int(el, 2)) for el in tmp]
+        tmp = "".join([el for el in tmp])
+        answer = tmp
+
+        tmpk = [key_bits[i:i + n] for i in range(0, len(key_bits), n)]
+        tmpk = [chr(int(el, 2)) for el in tmpk]
+        tmpk = "".join([el for el in tmpk])
+        key_to_return = tmpk
+
+    return answer, key_to_return
 
 
 def main():
     with open("test.txt", "r", encoding='utf8') as file, open("test_2.txt", "w", encoding='utf8') as w_file:
         message = file.read()
 
-        answer, key = process_vernam_cypher(text=message, key=None)
-        message, key = process_vernam_cypher(text=answer, key=key)
+        print("To cipher:", message)
         w_file.write(message)
+        w_file.write("\n")
+        answer, key = process_vernam_cypher(text=message, key=None)
+        print("Ciphered message:", answer)
+        print("Key:", key)
+        w_file.write(answer)
+        w_file.write("\n")
+        w_file.write(key)
+        w_file.write("\n")
+        message, key = process_vernam_cypher(text=answer, key=key)
+        print("Deciphered message:", message)
+        w_file.write(message)
+        w_file.write("\n")
     # print(encoded_message)
     # print(process_vernam_cypher(text=encoded_message, key=test_key))
 
